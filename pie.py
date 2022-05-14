07a4ff0781d7a5e64ebc4d3821839ec3e7bde3cd
bc10201 <bmym2010xh2@gmail.com>
2022-05-14 15:34:52 +0800

    Update Pie.py

--- pie.py
+++ pie.py
@@ -4,6 +4,8 @@ from reportlab.graphics import renderPDF
 from reportlab.graphics.shapes import Drawing
 import webbrowser,os
 c=canvas.Canvas("Time_data_card.pdf")
+plan = ["睡觉","上课","写作业","兴趣课","娱乐","吃饭"]
+data = [9,8,2,2,2,1]
 c.drawImage('card.png',0,0,width=372,height=665,mask=None)
 drawing=Drawing(372,665)
 pc = Pie()
@@ -11,6 +13,9 @@ pc.x = 115
 pc.y = 350
 pc.width = 150
 pc.height = 152
+pc.data = data
+pc.sideLabels = 1
+pc.labels = [str(1) for i in data]
 drawing.add(pc)
 renderPDF.draw(drawing,c,0,0)
 c.save()
